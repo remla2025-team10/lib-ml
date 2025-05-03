bbcab78ee38a64d061fb080a67988736f3f1fbc4
Sahar Marossi <marossi.sahar@gmail.com>
2025-05-03 13:15:50 +0200

    Initialized workflow config and boilerplate preprocessing package.

---
+++ preprocess_sentiment_analysis/preprocess.py
@@ -0,0 +1,7 @@
+# TODO: Add preprocessing
+
+def clean_text(text):
+    """
+    Strips leading/trailing whitespace + converts text to lowercase.
+    """
+    return text.strip().lower()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='preprocess-sentiment-analysis',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='REMLA 2025 Team 10',
+    description='Preprocessing logic for Restaurant Sentiment Analysis',
+    include_package_data=True,
+    python_requires='>3.8'
+)
